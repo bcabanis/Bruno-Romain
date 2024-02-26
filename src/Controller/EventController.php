be67a2dc39afad6e49211f8b2e0ab9d79bb7a2df
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-02-27 00:15:06 +0100

    Déplacement de l'outil de migration d'Event
    Déplacement de la classe Event dans une classe séparée

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -72,51 +72,6 @@ class EventController extends AbstractController
     }
 
 
-    // Envoie des évenements en BDD
-    #[Route('/to_bdd', name: 'app_event_bdd')]
-    public function EventToBDD(EventRepository $eventRepository): Response
-    {
-
-        // Récupération du lien de l'api (json)
-        $jsonData = './assets/json/event.json';
-
-        // Decodage en string pour la lecture en php
-        $eventsData = json_decode(file_get_contents($jsonData, true));
-
-        // Boucle sur $eventData pour permettre l'insertion des données de l'événements dans la BDD
-        foreach ($eventsData as $category => $events) {
-            foreach ($events as $event) {
-                foreach ($event as $e) {
-
-                    // Variable qui va permettre la vérification de non doublons dans la BDD
-                    $existingEvent = $eventRepository->findOneBy(['title' => $e->Titre]);
-
-                    // Condition pour eviter des doublons d'événements dans la base de données 
-                    if (!$existingEvent) {
-                        $eventDoc = new Events();
-                        $eventDoc->setCategory($category);
-                        $eventDoc->setTitle($e->Titre);
-                        $eventDoc->setDescription($e->Description);
-                        $eventDoc->setEventDate($e->Date_de_l_evenement);
-                        $eventDoc->setAddress($e->Adresse);
-                        $eventDoc->setImageUrl($e->URL_d_image);
-                        $eventDoc->seteventId($e->eventId);
-                        $eventDoc->setDateFormat($e->date_format);
-                        $eventDoc->setLong($e->Longitude);
-                        $eventDoc->setLat($e->Latitude);
-                        $eventDoc->setOrga($e->Organisateur);
-
-                        // Enregistre dans la base de données
-                        $eventRepository->save($eventDoc);
-                    }
-                }
-            }
-        }
-
-        // Réponse si bon injection des données
-        return new Response('Events inserted successfully!');
-    }
-
     #[Route('/{eventUid}', name: 'app_event_show')]
     public function show(ChatMessageRepository $chatMessageRepository, string $eventUid, SessionInterface $sessionInterface, NewApiService $newApi): Response
     {
